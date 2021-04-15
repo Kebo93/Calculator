4f1e66bb5f517622be2d790ce2af162772a921ea
Kebo93 <Kewin.DiFranco@outlook.it>
2021-04-15 15:55:11 +0200

    added sum, sub, divide, mult to calculator

--- src/it/kewin/calculator/Calculator.java
+++ src/it/kewin/calculator/Calculator.java
@@ -1,5 +1,19 @@
 package it.kewin.calculator;
 
 public class Calculator {
+	public int sum(int sx, int dx) {
+		return sx + dx;
+	}
 
+	public int sub(int sx, int dx) {
+		return sx - dx;
+	}
+
+	public float divide(int sx, int dx) {
+		return sx / dx;
+	}
+
+	public int mult(int sx, int dx) {
+		return sx * dx;
+	}
 }
